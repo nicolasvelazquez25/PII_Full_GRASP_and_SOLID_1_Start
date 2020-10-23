dacae6e9c3eb3b057bb33980443e43c4ea4e22e3
agusbenia25 <agusbenia@hotmail.com>
2020-10-23 15:35:59 -0300

    Comento la clase Step

--- src/Library/Step.cs
+++ src/Library/Step.cs
@@ -24,7 +24,23 @@ namespace Full_GRASP_And_SOLID.Library
 
         public Equipment Equipment { get; set; }
 
-        // Agregado por Expert
+        /*
+
+        Clase Step: aplica el patrón Expert
+
+        Aplica el patrón Expert ya que la clase Step posee toda la información necesaria para hacer 
+        el cálculo del precio de cada paso de la receta, 
+        esto es porque la clase Step esta formada por el producto usado, 
+        el cual brindará el costo por cada unidad, y
+        por el equipamiento, que brindará el costo por cada hora de uso. 
+        A esto se suman las variables de cantidad y tiempo que 
+        se multiplicarán por los valores anteriormente mencionados respectivamente.
+
+        De este modo es mas fácil que cada paso en sí calcule su precio para poder determinar 
+        el precio final en base a la suma de los mismos
+        sin operar toda la funcion en la clase Recipe dependiendo así completamente de ella. 
+
+        */
         public double GetStepCost()
         {    
             return (this.Input.UnitCost * this.Quantity) + (this.Equipment.HourlyCost * this.Time);
