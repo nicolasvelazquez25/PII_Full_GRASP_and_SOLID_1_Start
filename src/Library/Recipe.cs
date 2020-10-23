e56ef0e16bcc6a1b0d01aacbd42f7b33830e22c1
gonzalo prampini <gonzaloprampini@gmail.com>
2020-10-23 15:53:12 -0300

    agrego los comentarios de la clase Recipe

--- src/Library/Recipe.cs
+++ src/Library/Recipe.cs
@@ -35,7 +35,19 @@ namespace Full_GRASP_And_SOLID.Library
                     $"usando '{step.Equipment.Description}' durante {step.Time}");
             }
         }
-        // Agregado por Expert
+        /*
+
+        Clase Recipe: aplica el patrón Expert
+
+        Aplica el patrón Expert ya que la clase Recipe esta compuesta por un objeto de la
+        clase Step, por lo que puede acceder facilmente al cálculo de cada paso mediante la
+        función GetStepCost(), por lo que la función GetProductionCost() solo se encargara de
+        operar la sumatoria de los costos de cada paso. 
+
+        De este modo es mas fácil que cada paso en sí calcule su precio para poder determinar 
+        el precio final en base a la suma de los mismos sin operar toda la funcion en la clase Recipe dependiendo así completamente de ella. 
+
+        */
         public double GetProductionCost()
         {
             double result = 0;
